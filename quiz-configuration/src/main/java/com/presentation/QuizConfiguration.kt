8a28b631ec24291df69e9ea411df4080a2f5d112
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2023-02-03 23:22:44 +0300

    Implement fragment_quiz_configuration.xml

--- quiz-configuration/src/main/java/com/presentation/QuizConfiguration.kt
+++ quiz-configuration/src/main/java/com/presentation/QuizConfiguration.kt
@@ -1,10 +1,14 @@
 package com.presentation
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.di.ConfigurationScreenComponentViewModel
 import com.quiz_configuration.R
 import com.quiz_configuration.databinding.FragmentQuizConfigurationBinding
 
@@ -13,11 +17,17 @@ class QuizConfiguration : Fragment(R.layout.fragment_quiz_configuration) {
     private var _binding: FragmentQuizConfigurationBinding? = null
     private val binding get() = _binding!!
 
+    override fun onAttach(context: Context) {
+        ViewModelProvider(this).get<ConfigurationScreenComponentViewModel>()
+            .configurationComponent.inject(this)
+        super.onAttach(context)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentQuizConfigurationBinding.inflate(inflater,container,false)
+        _binding = FragmentQuizConfigurationBinding.inflate(inflater, container, false)
         return binding.root
     }
 
