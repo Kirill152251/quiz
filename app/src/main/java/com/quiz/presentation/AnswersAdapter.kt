f146557907117970109f362e81c0f31e3bebe375
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2023-02-28 22:00:32 +0300

    Implement result screen fragment logic

--- app/src/main/java/com/quiz/presentation/AnswersAdapter.kt
+++ app/src/main/java/com/quiz/presentation/AnswersAdapter.kt
@@ -1,30 +1,26 @@
 package com.quiz.presentation
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.quiz.R
 import com.quiz.databinding.AnswerItemBinding
 import com.quiz.domain.models.AnsweredQuestion
 
-class AnswersAdapter(
-    @ColorInt private val correctColor: Int,
-    @ColorInt private val inCorrectColor: Int,
-    @DrawableRes private val correctIcon: Int,
-    @DrawableRes private val inCorrectIcon: Int
-) :
+class AnswersAdapter :
     ListAdapter<AnsweredQuestion, AnswersAdapter.AnswerViewHolder>(ItemCallback) {
 
-    class AnswerViewHolder(val binding: AnswerItemBinding) :
+    class AnswerViewHolder(val binding: AnswerItemBinding, val context: Context) :
         RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnswerViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = AnswerItemBinding.inflate(inflater)
-        return AnswerViewHolder(binding)
+        return AnswerViewHolder(binding, parent.context)
     }
 
     override fun onBindViewHolder(holder: AnswerViewHolder, position: Int) {
@@ -33,20 +29,18 @@ class AnswersAdapter(
             val indexOfChosenAnswer = item.allAnswers.indexOf(item.chosenAnswer)
             val indexOfCorrectAnswer = item.allAnswers.indexOf(item.correctAnswer)
             if (item.isAnsweredCorrect()) {
-                itemAnswer.setCardBackgroundColor(correctColor)
-                setCorrectnessIcon(this, indexOfChosenAnswer, correctIcon)
+                setCorrectnessIcon(this, indexOfChosenAnswer, R.drawable.ic_correct)
             } else {
-                setCorrectnessIcon(this, indexOfCorrectAnswer, correctIcon)
-                setCorrectnessIcon(this, indexOfChosenAnswer, inCorrectIcon)
-                itemAnswer.setCardBackgroundColor(inCorrectColor)
+                setCorrectnessIcon(this, indexOfCorrectAnswer, R.drawable.ic_correct)
+                setCorrectnessIcon(this, indexOfChosenAnswer, R.drawable.ic_wrong)
             }
 
             textQuestion.text = item.questionText
 
-            textFirstAnswer.text = item.allAnswers[0]
-            textSecondAnswer.text = item.allAnswers[1]
-            textThirdAnswer.text = item.allAnswers[2]
-            textForthAnswer.text = item.allAnswers[3]
+            textFirstAnswer.text = holder.context.getString(R.string.first, item.allAnswers[0])
+            textSecondAnswer.text = holder.context.getString(R.string.second, item.allAnswers[1])
+            textThirdAnswer.text = holder.context.getString(R.string.third, item.allAnswers[2])
+            textForthAnswer.text = holder.context.getString(R.string.forth, item.allAnswers[3])
         }
     }
 
--- app/src/main/java/com/quiz/presentation/ResultScreen.kt
+++ app/src/main/java/com/quiz/presentation/ResultScreen.kt
@@ -6,14 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.quiz.R
+import com.quiz.appComponent
 import com.quiz.databinding.FragmentResultScreenBinding
+import com.quiz.presentation.view_models.ResultScreenViewModel
+import javax.inject.Inject
 
 class ResultScreen : Fragment() {
 
     private var _binding: FragmentResultScreenBinding? = null
     private val binding get() = _binding!!
 
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    private val viewModel by viewModels<ResultScreenViewModel> { viewModelFactory }
+
+    private val adapter = AnswersAdapter()
+
     override fun onAttach(context: Context) {
+        context.appComponent.inject(this)
         super.onAttach(context)
     }
 
@@ -27,6 +43,25 @@ class ResultScreen : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            rvAnswers.adapter = adapter
+            rvAnswers.layoutManager = LinearLayoutManager(requireContext())
+            rvAnswers.addItemDecoration(
+                DividerItemDecoration(
+                    requireContext(),
+                    LinearLayoutManager.VERTICAL
+                )
+            )
+            textResult.text = getString(
+                R.string.quiz_result,
+                viewModel.getNumberOfCorrectAnswers(),
+                viewModel.getNumberOfQuestions()
+            )
+            btnStartNewQuiz.setOnClickListener {
+                findNavController().navigate(R.id.action_resultScreen_to_quizConfiguration)
+            }
+        }
+        adapter.submitList(viewModel.getAnsweredQuestions())
     }
 
     override fun onDestroy() {
