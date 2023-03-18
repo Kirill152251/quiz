6f0bd6ae5d2186ca642b146bc2c884d7bed4a736
Kirill Ermalenok <kirya.ermalenok2001@gmail.com>
2023-03-18 12:15:46 +0300

    Write more DB tests

--- app/src/androidTest/java/com/quiz/data/cache/QuizDatabaseTest.kt
+++ app/src/androidTest/java/com/quiz/data/cache/QuizDatabaseTest.kt
@@ -4,6 +4,7 @@ import android.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
 import app.cash.turbine.test
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.*
@@ -15,6 +16,7 @@ import org.junit.runner.RunWith
 
 @OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(AndroidJUnit4::class)
+@SmallTest
 class QuizDatabaseTest {
 
     @get: Rule
@@ -31,6 +33,24 @@ class QuizDatabaseTest {
         questions = listOf(),
         saveTime = "00"
     )
+
+    private val entity1 = QuizEntity(
+        id = "2",
+        difficulty = "hard",
+        numberOfQuestions = 1,
+        category = listOf("category"),
+        questions = listOf(),
+        saveTime = "00"
+    )
+    private val entity2 = QuizEntity(
+        id = "3",
+        difficulty = "hard",
+        numberOfQuestions = 1,
+        category = listOf("category"),
+        questions = listOf(),
+        saveTime = "00"
+    )
+
 
     @Before
     fun setup() {
@@ -58,6 +78,21 @@ class QuizDatabaseTest {
     }
 
     @Test
+    fun add_tree_items_should_return_this_items_inFlow() = runTest {
+        dao.insert(entity)
+        dao.insert(entity1)
+        dao.insert(entity2)
+        dao.getData().test {
+            val list = awaitItem()
+            assert(list.size == 3)
+            assert(list[0].id == "1")
+            assert(list[1].id == "2")
+            assert(list[2].id == "3")
+            cancel()
+        }
+    }
+
+    @Test
     fun add_two_same_entities_should_save_only_one() = runTest {
         dao.insert(entity)
         val id = dao.insert(entity)
@@ -65,6 +100,7 @@ class QuizDatabaseTest {
             val list = awaitItem()
             assert(id == -1L)
             assert(list.size == 1)
+            cancel()
         }
     }
 
@@ -75,6 +111,20 @@ class QuizDatabaseTest {
         dao.getData().test {
             val list = awaitItem()
             assert(list.isEmpty())
+            cancel()
+        }
+    }
+
+    @Test
+    fun add_tree_items_delete_oneOfThem_flow_should_contain_it() = runTest {
+        dao.insert(entity)
+        dao.insert(entity1)
+        dao.insert(entity2)
+        dao.delete(entity2)
+        dao.getData().test {
+            val list = awaitItem()
+            assert(!list.contains(entity2))
+            cancel()
         }
     }
 }
